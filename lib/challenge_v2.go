6b170cc2af14846cd534b55ddeb4c7bab0d7ee23
Advik <advik.b@gmail.com>
2025-07-08 14:57:27 +0530

    Update challenge_v2.go

--- lib/challenge_v2.go
+++ lib/challenge_v2.go
@@ -3,67 +3,114 @@ package cloudscraper
 import (
 	"fmt"
 	"log"
+	"net/url"
 	"regexp"
 	"strings"
 
 	"github.com/Advik-B/cloudscraper/lib/js"
 )
 
-// Regex to find and extract the modern challenge script content.
-var v2ScriptRegex = regexp.MustCompile(`(?s)<script[^>]*>(.*?window\._cf_chl_opt.*?)<\/script>`)
+// Use a simple, robust regex to capture the content of ANY script tag.
+var v2ScriptRegex = regexp.MustCompile(`(?s)<script[^>]*?>(.*?)</script>`)
 
-// solveV2Logic solves modern v2/v3 challenges by delegating to the appropriate JS engine implementation.
-func solveV2Logic(body, domain string, engine js.Engine, logger *log.Logger) (string, error) {
-	scriptMatches := v2ScriptRegex.FindAllStringSubmatch(body, -1)
-	if len(scriptMatches) == 0 {
-		return "", fmt.Errorf("could not find modern JS challenge scripts")
+// --- CHANGE 1: Update function signature to accept a full *url.URL ---
+func solveV2Logic(body string, pageURL *url.URL, engine js.Engine, logger *log.Logger) (string, error) {
+	allMatches := v2ScriptRegex.FindAllStringSubmatch(body, -1)
+	if len(allMatches) == 0 {
+		return "", fmt.Errorf("could not find any JS scripts on challenge page")
 	}
 
-	// Use a special synchronous path for Otto, which can't handle async setTimeout.
+	var challengeScripts [][]string
+	for _, match := range allMatches {
+		if len(match) > 1 && (strings.Contains(match[0], "challenge-platform") || strings.Contains(match[1], "_cf_chl_opt")) {
+			challengeScripts = append(challengeScripts, match)
+		}
+	}
+
+	if len(challengeScripts) == 0 {
+		return "", fmt.Errorf("could not find the primary Cloudflare JS challenge script")
+	}
+
 	if ottoEngine, ok := engine.(*js.OttoEngine); ok {
-		return ottoEngine.SolveV2Challenge(body, domain, scriptMatches, logger)
+		// Note: Otto will likely fail this advanced challenge, but we keep the path for compatibility.
+		return ottoEngine.SolveV2Challenge(body, pageURL.Host, challengeScripts, logger)
 	}
 
-	// Use a modern asynchronous path for external runtimes (node, deno, bun).
-	return solveV2WithExternal(domain, scriptMatches, engine)
+	// --- CHANGE 2: Pass the full URL object to the external solver ---
+	return solveV2WithExternal(pageURL, challengeScripts, engine)
 }
 
-// solveV2WithExternal builds a full script with shims and an async callback to solve the challenge.
-func solveV2WithExternal(domain string, scriptMatches [][]string, engine js.Engine) (string, error) {
-	// This DOM shim is required for the challenge script to run in a non-browser environment.
-	atobImpl := `
-        var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
-        var a, b, c, d, e, f, g, i = 0, result = '';
-        str = str.replace(/[^A-Za-z0-9\+\/\=]/g, '');
-        do {
-            a = chars.indexOf(str.charAt(i++)); b = chars.indexOf(str.charAt(i++));
-            c = chars.indexOf(str.charAt(i++)); d = chars.indexOf(str.charAt(i++));
-            e = a << 18 | b << 12 | c << 6 | d; f = e >> 16 & 255; g = e >> 8 & 255; a = e & 255;
-            result += String.fromCharCode(f);
-            if (c != 64) result += String.fromCharCode(g);
-            if (d != 64) result += String.fromCharCode(a);
-        } while (i < str.length);
-        return result;
-    `
+// --- CHANGE 3: Update signature and implement a new, powerful DOM shim ---
+func solveV2WithExternal(pageURL *url.URL, scriptMatches [][]string, engine js.Engine) (string, error) {
+	// A comprehensive DOM shim that defines `location` dynamically and intercepts dynamic script loading.
+	setupScript := fmt.Sprintf(`
+		(function (global) {
+			var window = global;
+			global.self = window;
+			global.window = window;
+			global.top = window;
+			global.parent = window;
 
-	setupScript := `
-		var window = globalThis;
-		var navigator = { userAgent: "" };
-		var document = {
-			getElementById: function(id) {
-				if (!this.elements) this.elements = {};
-				if (!this.elements[id]) this.elements[id] = { value: "" };
-				return this.elements[id];
-			},
-			createElement: function(tag) {
-				return {
-					firstChild: { href: "https://` + domain + `/" }
-				};
-			},
-			cookie: ""
-		};
-		var atob = function(str) {` + atobImpl + `};
-	`
+			global.location = {
+				href: "%s",
+				protocol: "%s:",
+				host: "%s",
+				hostname: "%s",
+				port: "%s",
+				pathname: "%s",
+				search: "?%s",
+				hash: "%s",
+				assign: function() {},
+				reload: function() {},
+				replace: function() {}
+			};
+
+			global.navigator = {
+				userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+				platform: "Win32",
+				language: "en-US"
+			};
+
+			global.history = {
+				replaceState: function() {}
+			};
+
+			global.document = {
+				elements: {},
+				getElementById: function(id) {
+					if (!this.elements[id]) { this.elements[id] = { value: "" }; }
+					return this.elements[id];
+				},
+				createElement: function(tag) {
+					return { setAttribute: function() {}, src: "" };
+				},
+				getElementsByTagName: function(name) {
+					if (name === 'head' || name === 'body') {
+						return [{
+							appendChild: function(element) {
+								// CRITICAL: Intercept and neutralize dynamic script loading.
+							}
+						}];
+					}
+					return [];
+				},
+				cookie: ""
+			};
+
+			var atobImpl = 'var chars="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";var a,b,c,d,e,f,g,i=0,result="";str=str.replace(/[^A-Za-z0-9\\+\\/\\=]/g,"");do{a=chars.indexOf(str.charAt(i++));b=chars.indexOf(str.charAt(i++));c=chars.indexOf(str.charAt(i++));d=chars.indexOf(str.charAt(i++));e=a<<18|b<<12|c<<6|d;f=e>>16&255;g=e>>8&255;a=e&255;result+=String.fromCharCode(f);if(c!=64)result+=String.fromCharCode(g);if(d!=64)result+=String.fromCharCode(a)}while(i<str.length);return result;';
+			global.atob = new Function('str', atobImpl);
+
+		})(typeof globalThis !== 'undefined' ? globalThis : this);
+	`,
+		pageURL.String(),
+		pageURL.Scheme,
+		pageURL.Host,
+		pageURL.Hostname(),
+		pageURL.Port(),
+		pageURL.Path,
+		pageURL.RawQuery,
+		pageURL.Fragment,
+	)
 
 	var fullScript strings.Builder
 	fullScript.WriteString(setupScript)
@@ -71,22 +118,19 @@ func solveV2WithExternal(domain string, scriptMatches [][]string, engine js.Engi
 	for _, match := range scriptMatches {
 		if len(match) > 1 {
 			scriptContent := match[1]
-			// The script expects a 'challenge-form' to exist for submission. We stub it.
 			scriptContent = strings.ReplaceAll(scriptContent, `document.getElementById('challenge-form');`, "({})")
 			fullScript.WriteString(scriptContent)
 			fullScript.WriteString(";\n")
 		}
 	}
 
-	// The Cloudflare script uses a setTimeout of 4000ms. We'll wait a little longer
-	// and then extract the answer, printing it to stdout for Go to capture.
 	answerExtractor := `
         setTimeout(function() {
             try {
                 var answer = document.getElementById('jschl-answer').value;
                 console.log(answer);
             } catch (e) {
-                // Ignore errors if the element isn't found, the process will just exit.
+                // Ignore errors if the element isn't found.
             }
         }, 4100);
     `
